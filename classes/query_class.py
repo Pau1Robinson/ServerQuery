d20b52a44992874cc016a2110facb3d8eed395a0
Pau1Robinson <paul@Paul-PC.localdomain>
2019-10-18 17:00:12 +1100

    impleneted api get requests

--- classes/query_class.py
+++ classes/query_class.py
@@ -1,6 +1,7 @@
 import time
 import os
 import requests
+import json
 import valve.source
 import valve.source.a2s
 import valve.source.master_server
@@ -10,10 +11,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 from requests.exceptions import HTTPError
 
-load_dotenv()
+# load_dotenv()
 API_KEY = os.getenv("API_KEY")
 
 class SteamQuery():
@@ -65,7 +66,7 @@ class SteamQuery():
         '''
         fix me
         '''
-        self.rate_limit()
+        A2s_success = True
         try:
             with valve.source.a2s.ServerQuerier(self.server_ip) as server:
                 info = server.info()
@@ -73,93 +74,126 @@ class SteamQuery():
 
         except valve.source.NoResponseError:
             print("Server {}:{} timed out!".format(*self.server_ip))
+            A2s_success = False
 
-        self.json_data["server_info"]["server_name"] = ("{server_name}".format(**info))
-        self.json_data["server_info"]["player_count"] = ("{player_count}".format(**info))
-        self.json_data["server_info"]["max_players"] = ("{max_players}".format(**info))
-        self.json_data["server_info"]["server_map"] = ("{map}".format(**info))
-        self.json_data["server_info"]["server_game"] = ("{game}".format(**info))
-        self.json_data["server_info"]["server_type"] = ("{server_type}".format(**info))
-        for player in sorted(players["players"], key=lambda p: p["score"], reverse=True):
-            if "{name}".format(**player) != '':
-                self.json_data["player_info"]["{name}".format(**player)] = {
-                    "steam_id":"",
-                    "profile_url":"",
-                    "avatar_url":"",
-                    "profile_vis":""
-                }
+        if A2s_success is True:
+            self.json_data["server_info"]["server_name"] = ("{server_name}".format(**info))
+            self.json_data["server_info"]["player_count"] = ("{player_count}".format(**info))
+            self.json_data["server_info"]["max_players"] = ("{max_players}".format(**info))
+            self.json_data["server_info"]["server_map"] = ("{map}".format(**info))
+            self.json_data["server_info"]["server_game"] = ("{game}".format(**info))
+            self.json_data["server_info"]["server_type"] = ("{server_type}".format(**info))
+            for player in sorted(players["players"], key=lambda p: p["score"], reverse=True):
+                if "{name}".format(**player) != '':
+                    self.json_data["player_info"]["{name}".format(**player)] = {
+                        "steam_id":"",
+                        "profile_url":"",
+                        "avatar_url":"",
+                        "profile_vis":""
+                    }
+        else:
+            print('A2s query failed')
 
     def fill_player_url():
         for dict in self.json_data["player_info"]:
             pass
 
-    def search_player_name(self, name):
+    def init_browser(self):
+        option = webdriver.ChromeOptions()
+        option.add_argument("--incognito")
+        browser = webdriver.Chrome(executable_path=r'C:/Program Files (x86)/Google/Chrome/Application/chromedriver.exe', chrome_options=option)
+        return browser
+
+    def close_browser(self, browser):
+        browser.quit()
+
+    def search_player_name(self, name, browser):
         '''
         fix me
         '''
         name = name.replace(" ", "+")
-        option = webdriver.ChromeOptions()
-        option.add_argument("--incognito")
-        browser = webdriver.Chrome(executable_path=r'C:/Program Files (x86)/Google/Chrome/Application/chromedriver.exe', chrome_options=option)
         browser.get(f"https://steamcommunity.com/search/users/#text={name}")
-        timeout = 20
+        timeout = 2
+        selenium_success = True
         try:
             WebDriverWait(browser, timeout).until(EC.visibility_of_element_located((By.CLASS_NAME, 'searchPersonaName')))
         except TimeoutException:
             print("Timed out waiting for page to load")
-            browser.quit()
+            selenium_success = False
 
-        html = browser.page_source
-        soup = BeautifulSoup(html, 'html.parser')
-        resultsRow = soup.find_all('a', {'class': 'searchPersonaName'})
-        results = []
+        if selenium_success is True:
+            html = browser.page_source
+            browser.get("about:blank")
+            soup = BeautifulSoup(html, 'html.parser')
+            resultsRow = soup.find_all('a', {'class': 'searchPersonaName'})
+            results = []
 
-        for resultRow in resultsRow:
-            steam_name = resultRow.getText()
-            steam_URL = resultRow.get('href')
-            results.append({
-            "name": steam_name,
-            "steam_url": steam_URL
-            })
+            for resultRow in resultsRow:
+                steam_name = resultRow.getText()
+                steam_URL = resultRow.get('href')
+                results.append({
+                "name": steam_name,
+                "steam_url": steam_URL
+                })
 
-        num_match = 0
-        name = name.replace("+", " ")
-        for dict in results:
-            if dict["name"] == name:
-                num_match += 1
-                match_url = dict["steam_url"]
-        if num_match == 1:
-            return match_url
-        elif num_match == 0:
-            return "No steam profile match found"
-        elif num_match > 1:
-            return "Steam name was too common"
+            num_match = 0
+            name = name.replace("+", " ")
+            for dict in results:
+                if dict["name"] == name:
+                    num_match += 1
+                    match_url = dict["steam_url"]
+            if num_match == 1:
+                return match_url
+            elif num_match == 0:
+                return "No steam profile match found"
+            elif num_match > 1:
+                return "Steam name was too common"
+        return "Timed out waiting for page to load"
 
     def get_steam_id(self, url):
         '''
         fix me
         '''
-        self.rate_limit()
+        url_list = url.split('/')
+        if len(url_list[-1]) == 17 and url_list[-1].isdigit() is True:
+            return url_list[-1]
         try:
-            steam_id = requests.get(f'http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key={API_KEY}&vanityurl={url}')
+            request_id = requests.get(f'http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key={API_KEY}&vanityurl={url}&format=json')
         except HTTPError as http_err:
-            return(f'Unable to resolve steam url into id http error:{http_err}')
+            return(f'Unable to resolve steam url into id')
         except Exception as err:
             return('Unable to resolve steam url into id')
-        return steam_id.content
+        json_data = json.loads(request_id.content)
+        if json_data['response']['success'] == '1':
+            steam_id = json_data['response']['steamid']
+        else:
+            return('Unable to resolve steam url into id')
+        return steam_id
 
     def fill_player_steam_info(self, steam_id):
         '''
         fix me
         '''
-        self.rate_limit()
+        steam_query_success = True
         try:
             steam_id = requests.get(f'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={API_KEY}&steamids={steam_id}&format=json')
         except HTTPError as http_err:
             return(f'Unable to resolve steam url into id http error:{http_err}')
+            steam_query_success = False
         except Exception as err:
             return('Unable to resolve steam url into id')
-        return steam_id.content
+            steam_query_success = False
+        if steam_query_success is True:
+            json_data = json.loads(steam_id.content)
+            avatar_url = json_data['response']['players'][0]['avatar']
+            if json_data['response']['players'][0]['communityvisibilitystate'] == 1:
+                profile_vis = 'hidden'
+            elif json_data['response']['players'][0]['communityvisibilitystate'] == 3:
+                profile_vis = 'public'
+            else:
+                profile_vis = 'not found'
+            return (avatar_url, profile_vis)
+        return ("unable to query Api", "unable to query Api")
 
     def print_json(self):
         '''
@@ -167,10 +201,14 @@ class SteamQuery():
         '''
         print(self.json_data)
 
-query = SteamQuery(('45.121.209.68', 26015), 1)
-# print(query.search_player_name("Who's+Bishop"))
+# query = SteamQuery(('45.121.209.68', 26015), 1)
+# browser = query.init_browser()
+# print(query.search_player_name("(DoW)+WilboWD40", browser))
+# query.rate_limit()
+# print(query.search_player_name("About_30_ninjas", browser))
+# query.close_browser(browser)
 # print(query.get_steam_id("DarkyBenny"))
-print(query.fill_player_steam_info("76561198151637619"))
+# print(query.fill_player_steam_info("76561198151637619"))
 # query.init_json()
 # query.get_A2s()
 # query.print_json()
--- server_query.py
+++ server_query.py
@@ -1,29 +1,27 @@
 import os
-from flask import render_template
+import requests
 from flask import Flask
-from flask import request
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-class QueryForm(FlaskForm):
-    query = StringField('Server query', validators=[DataRequired()])
-    submit = SubmitField('Enter Ip')
+from classes import query_class
+from flask import jsonify
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('CSRF_KEY')
 
-@app.route('/')
-@app.route('/index')
-def index():
-    user = {'username': 'Test'}
-    return render_template('index.html', title='Home', user=user)
-
-@app.route('/query')
-def query():
-    form = QueryForm()
-    return render_template('query.html', title='Query', form=form)
-
-@app.route('/query=<server_ip>')
-def displayServer(server_ip):
-    
+@app.route('/api/<server_ip>/', methods=["GET"])
+def index(server_ip):
+    query = query_class.SteamQuery((server_ip, 28215), 1)
+    query.init_json()
+    query.get_A2s()
+    browser = query.init_browser()
+    for player in query.json_data['player_info']:
+        query.rate_limit()
+        player_url = query.search_player_name(player, browser)
+        query.rate_limit()
+        player_steam_id = query.get_steam_id(player_url)
+        query.json_data['player_info'][player]['steam_id'] = player_steam_id
+        query.json_data['player_info'][player]['profile_url'] = player_url
+        if player_steam_id != "Unable to resolve steam url into id":
+            steam_query_data = query.fill_player_steam_info(player_steam_id)
+            query.json_data['player_info'][player]['avatar_url'] = steam_query_data[0]
+            query.json_data['player_info'][player]['profile_vis'] = steam_query_data[1]
+    query.close_browser(browser)
+    return jsonify(query.json_data)
